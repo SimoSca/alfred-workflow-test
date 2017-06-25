e00e15be9125c36bd5a4b486603befd3c796fca5
Simone Scardoni <simonescardoni@MacBook-Enomis.local>
2017-06-25 12:30:12 +0200

    [new] alfred mysql workflow

---
+++ find_files.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once("workflows.php");
+
+
+class FindQueriesFiles {
+
+    private $matchList = [];
+
+    public function __construct($projectFolder, $query){
+        
+        // used into returnUnique
+        $this->defaultFile = $projectFolder . '/' . $query;
+
+        $files = scandir($projectFolder);
+        
+        foreach($files as $file) {    
+            //do your work here
+            if( preg_match('/queries/i', $file) && preg_match('/'.$query.'/i', $file) ){
+
+                $count = count( $this->matchList );
+
+                $this->matchList[] = [
+                    "uid" => $count . "-PM",
+                    "type"=> "file",
+                    "title"=> $file,
+                    "subtitle"=> "is_file",
+                    "arg"=> $projectFolder .'/' . $file,
+                    "autocomplete"=> $file,
+                    "icon" => 'icon.png',
+                    "valid" => "no"
+                ];
+            }
+        } 
+
+        
+
+    }
+
+
+    public function countMatch(){
+        return count( $this->matchList );
+    }
+
+    public function printList(){
+
+        $wf = new Workflows();
+
+        if($this->countMatch() == 0) {
+            $wf->result("999","","No Project Type matches \$clean!", "Project Manager", 'icon.png', 'no');
+        }else{
+            foreach($this->matchList as $item){
+                $wf->result( $item["uid"] ,$item["arg"] ,$item["title"] , $item["subtitle"], $item["icon"], $item["valid"], $item["autocomplete"] );
+            }
+        }
+
+        echo $wf->toxml();
+    }
+
+    public function returnUnique(){
+        return $this->countMatch[0] ? $this->countMatch[0] : $this->defaultFile ;
+    }
+
+}// end class
+ 
---
+++ parse_file.php
@@ -0,0 +1,81 @@
+<?php
+
+require_once("workflows.php");
+
+class ParseQueriesFile {
+
+    private $matchList = [];
+
+    public function __construct($file){
+        
+        // var_dump('file', $file);
+
+        $text = file_get_contents($file);
+        
+        preg_match_all('/#### *([^\r?\n]+)\r?\n(.+)````[^\r?\n]*\r?\n(.*)````/misU', $text, $matches);
+
+        // var_dump('match', $matches);
+
+        $sections = $this->sectionize($matches);
+        // var_dump('secs', $sections);
+        $this->generateMatchList($sections);
+    }
+
+    private function sectionize($matches){
+        $sections = [];
+        foreach($matches[1] as $key => $val){
+            $sections[] = [
+                'title' => trim($matches[1][$key]),
+                'description' => trim($matches[2][$key]),
+                'code' => trim($matches[3][$key])
+            ];
+        }
+        return $sections;
+    }
+
+    private function generateMatchList($sections){
+
+        foreach($sections as $section){
+            $count = count( $this->matchList );
+            // var_dump($this->matchList);
+
+                $this->matchList[] = [
+                    "uid" => $count . "-PM",
+                    "type"=> "file",
+                    "title"=> $section['title'],
+                    "subtitle"=> $section['description'],
+                    "arg"=> $section['code'],
+                    "autocomplete"=> '...',
+                    "icon" => 'icon.png',
+                    "valid" => "yes"
+                ];
+        }
+
+    }
+
+    public function countMatch(){
+        return count( $this->matchList );
+    }
+
+    public function printList(){
+
+        // var_dump($this->matchList);
+
+        $wf = new Workflows();
+
+        if($this->countMatch() == 0) {
+            $wf->result("999","","File has not queries...", "bad file", 'icon.png', 'no');
+        }else{
+            foreach($this->matchList as $item){
+                $wf->result( $item["uid"] ,$item["arg"] ,$item["title"] , $item["subtitle"], $item["icon"], $item["valid"], $item["autocomplete"] );
+            }
+        }
+
+        echo $wf->toxml();
+    }
+
+
+}// end class
+
+//new ParseQueriesFile('/Users/simonescardoni/Working/wlogo/Mixed/mysql/queries.md');
+
